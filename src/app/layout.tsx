1edc1870f97903c3c1f3c56467e20790a339b4ca
widi <gede.widyadnyana@taksu.tech>
2024-05-10 00:27:05 +0800

    fix bugs build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Lato } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/component/NavBarComponent";
 
